2727c9de518b3c2505219df187431dcded2454da
SusmoyBarman1 <susmoy.barman.bd@gmail.com>
2021-01-17 17:28:35 +0600

    Class added in model.py

--- Practice/susmoy/mysite/models.py
+++ Practice/susmoy/mysite/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class Work:
+	id: int
+	first_name: str
+	last_name: str
+	img: str
+	caption: str
--- Practice/susmoy/mysite/views.py
+++ Practice/susmoy/mysite/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
+from .models import Work
 
 # Create your views here.
 def index(request):
-	return render(request, 'mysite/index.html')
+
+	work1 = Work()
+	work1.first_name = 'Computer'
+	work1.last_name = 'Vision'
+	work1.caption = 'I am working on CNN and Computer vision for 1 year.'
+
+	return render(request, 'mysite/index.html', {'work1': work1})
