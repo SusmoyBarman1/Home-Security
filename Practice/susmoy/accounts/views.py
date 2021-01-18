cf443f7de09ace4dffe3c8e28cb5f5e39b738f01
SusmoyBarman1 <susmoy.barman.bd@gmail.com>
2021-01-18 21:36:52 +0600

    login added

--- Practice/susmoy/accounts/views.py
+++ Practice/susmoy/accounts/views.py
@@ -1,7 +1,29 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User, auth
+from django.contrib import messages
 
 # Create your views here.
+def login(request):
+
+	if request.method == 'POST':
+
+		username = request.POST['username']
+		password = request.POST['password']
+
+		user = auth.authenticate(username=username, password=password)
+
+		if user is not None:
+			auth.login(request, user)
+			return redirect('/')
+
+		else:
+			messages.info(request, 'Invalid User')
+			return redirect('login')
+
+	else:
+		return render(request, 'accounts/login.html')
+
+
 def register(request):
 
 	if request.method == 'POST':
@@ -15,19 +37,38 @@ def register(request):
 
 		if password1 == password2:
 			if User.objects.filter(username=username).exists():
+				messages.info(request, 'Username Taken...')
 				print('\n\n\n\nUsername Taken...\n\n\n\n')
+
+				return redirect('register')
+
 			elif User.objects.filter(email=email).exists():
+				messages.info(request, 'Email Taken...')
 				print('\n\n\n\nEmail Taken...\n\n\n\n')
+
+				return redirect('register')
+				
 			else:
 				user = User.objects.create_user(username=username, password=password1,email=email, first_name=first_name, last_name=last_name)
 				user.save();
 				print('\nUser Created!!!!')
+				return redirect('login')
 
 		else:
+			messages.info(request, 'Password not matching...')
 			print('\n\n\n\nPassword not matching...\n\n\n\n')
+
+			return redirect('register')
+				
 
 		return redirect('/')
 
 
 	else:
 		return render(request, 'accounts/register.html')
+
+
+def logout(request):
+	auth.logout(request)
+	return redirect('/')
+
