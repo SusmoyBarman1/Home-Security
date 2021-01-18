4b3c2093caf23c88a6ec27aa0e7464594a26f3ba
SusmoyBarman1 <susmoy.barman.bd@gmail.com>
2021-01-18 14:29:23 +0600

    dynamic content in html added

--- Practice/susmoy/mysite/views.py
+++ Practice/susmoy/mysite/views.py
@@ -7,6 +7,39 @@ def index(request):
 	work1 = Work()
 	work1.first_name = 'Computer'
 	work1.last_name = 'Vision'
+	work1.img = '01.jpg'
 	work1.caption = 'I am working on CNN and Computer vision for 1 year.'
 
-	return render(request, 'mysite/index.html', {'work1': work1})
+	work2 = Work()
+	work2.first_name = 'Natural'
+	work2.last_name = 'Language Processing'
+	work2.img = '02.jpg'
+	work2.caption = 'NLP is old..'
+
+	work3 = Work()
+	work3.first_name = 'Speech'
+	work3.last_name = 'Recognition'
+	work3.img = '03.jpg'
+	work3.caption = 'Speech Recognition is so complex'
+
+	work4 = Work()
+	work4.first_name = 'Computer'
+	work4.last_name = 'Science'
+	work4.img = '04.jpg'
+	work4.caption = 'I love cse'
+
+	work5 = Work()
+	work5.first_name = 'Electric'
+	work5.last_name = 'Engineering'
+	work5.img = '05.jpg'
+	work5.caption = 'EEE is bad'
+
+	work6 = Work()
+	work6.first_name = 'Information'
+	work6.last_name = 'Technology'
+	work6.img = '06.jpg'
+	work6.caption = 'ICE is not bad'
+
+	works = [work1, work2, work3, work4, work5, work6]
+
+	return render(request, 'mysite/index.html', {'works': works})
